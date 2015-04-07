e9c7592ad200b0070270a322a1b5e802257765c3
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-07 16:26:08 -0400

    got server running

--- server.rb
+++ server.rb
@@ -10,32 +10,41 @@ loop do                                             # Server runs forever
   client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
 
   lines = []
-  while (line = client.gets.chomp) && !line.empty?  # Read the request and collect it until it's empty
-    lines << line
+  while (line = client.gets) && !line.chomp.empty?  # Read the request and collect it until it's empty
+    lines << line.chomp
   end
-  puts lines                                        # Output the full request to stdout
+  puts lines
 
-  # index.html HTTP/1.1
-  # index.html
+  if lines[0]
+    filename = lines[0].gsub(/GET \//, '')
+                          .gsub(/\ HTTP.*/, '')
 
-  filename = lines[0].gsub(/GET \//, '').gsub(/ HTTP.*/, '')
-  headers = []
+    if File.exists?(filename)
+      puts "It worked!!"
+      response_body = File.read(filename)
+      success_header = [ ]
+    	success_header << "HTTP/1.1 200 OK"
+    	success_header << "Content-type: text/html"
+      success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+      success_header << "Connection: close"
+      header = success_header.join("\r\n")
+    else
+      puts "No ok!"
+      response_body = "File Not Found\n" # need to indicate end of the string with \n
+    	not_found_header = [ ]
+    	not_found_header << "HTTP/1.1 404 Not Found"
+      not_found_header << "Content-Type: text/plain" # is always text/plain
+      not_found_header << "Content-Length: #{response_body.length}" # should the actual size of the response body
+      not_found_header << "Connection: close"
+      header = not_found_header.join("\r\n")
+    end
 
-  if File.exists?(filename)
-  	body = File.read(filename)
-  	headers << "HTTP/1.1 200 OK"
-  	headers << "Content-type: text/html"
+    response = [header, response_body].join("\r\n\r\n")
+
+    client.puts(response)		                        # Output the current time to the client
   else
-  	body = "File not found\n"
-  	headers << "HTTP/1.1 404 Not Found"
-  	headers << "Content-type: text-plain"
+    puts "Empty request?"
   end
 
-  headers << "Content-Length"
-  headers << "Connection: close"
-  headers = headers.join("\r\n")
-  response = [headers, body].join("\r\n\r\n")
-
-  client.puts(response)		                        # Output the current time to the client
-  client.close                                      # Disconnect from the client
+  client.close                                    # Disconnect from the client
 end
